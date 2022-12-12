64526451b9a9d8ba233a5cd80000fe2e5c90bc3e
Caleb Dewey <calebdewey@Calebs-MacBook-Pro.local>
2022-12-12 14:33:09 -0700

    added guessing game function

---
+++ app.js
@@ -0,0 +1,10 @@
+var secretNumber = 8
+const parseInt(guess) = prompt("Guess a number between 1 and 10") 
+    if (guess === secretNumber) {
+        alert("Correct!")
+    } else if (guess < secretNumber) {
+        alert("Higher")
+    } else if (guess > secretNumber) {
+        alert("Lower")
+    }
+
